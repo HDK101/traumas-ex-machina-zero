39fa3c217a76b92fc3d7acac4ee4d791b0682b63
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-09-19 20:48:42 -0300

    chore: pixi ui

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,24 @@
 import * as PIXI from 'pixi.js';
+import { Button } from '@pixi/ui';
 import Game from './game';
 import Polygon from './polygon';
 
 import './style.css'
+import {Container} from 'pixi.js';
 
 const app = new PIXI.Application({
   resizeTo: window,
 });
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+const button = new Button(
+      new PIXI.Graphics()
+          .beginFill(0xFFFFFF)
+          .drawRoundedRect(0, 0, 100, 50, 15)
+);
+
+container.addChild(button.view);
 
 const webSocket = new WebSocket("ws://localhost:13200");
 
