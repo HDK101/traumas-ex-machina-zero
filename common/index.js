480987757317e2a8085c0957421e3586b1d6421a
Lucas Hideki Yamaguti Maziero <lucashideki87@gmail.com>
2023-09-27 23:16:55 -0300

    chore: nested packages

---
+++ common/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.walter = void 0;
+exports.walter = "walter";
--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,6 @@
 import Server from "./game/server.js";
+import { walter } from "../../common/index.js";
+console.log(walter);
 const server = new Server();
 function serverLoop() {
     server.process();
